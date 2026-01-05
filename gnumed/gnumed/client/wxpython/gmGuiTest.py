52d8a2a0ceac2cd122f5d7e6218e2cb56eb50320
DyuCho <dyumaan.choudhry@gmail.com>
2026-01-04 23:43:48 -0500

    Update gmGuiTest.py

--- gnumed/gnumed/client/wxpython/gmGuiTest.py
+++ gnumed/gnumed/client/wxpython/gmGuiTest.py
@@ -102,6 +102,7 @@ def setup_widget_test_env(patient=-1):
 #------------------------------------------------------------------------------
 def __activate_patient(patient=None):
 	if patient is None:
+		setup_widget_test_env(4)
 		return True
 		#.
 	if patient == -1:
